a90be6d90a5a7e7268aac4a5cd3d2720cd763cdc
dssgabriel <dss.gabriel@protonmail.com>
2023-06-23 00:29:40 +0200

    Correction proposal for the 2023 exam (session 1)

---
+++ exams/2023-session1/src/lib.rs
@@ -0,0 +1,15 @@
+//! This crate is a correction proposal for the 2023 exam (first session) of the IN512 - Systems
+//! Programming class at University of Versailles St-Quentin-en-Yvelines (UVSQ), France.
+//!
+//! It is divided into three modules (one per exercise), which implement a correction proposal.
+//!
+//! Follow the links below to access the documentation for each exercise:
+//! - [Exercise 1 - Polynomial evaluation](crate::polynomial_evaluation)
+//! - [Exercise 2 - Find the error](crate::find_the_error)
+//! - [Exercise 3 - More polynomials](crate::more_polynomials)
+
+#![feature(test)]
+
+pub mod find_the_error;
+pub mod more_polynomials;
+pub mod polynomial_evaluation;
